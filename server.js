da5f740d4a9290a263d4bdf85f5b1f1078eb1945
salimrached <59498439+salimrached@users.noreply.github.com>
2025-07-27 17:07:11 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', {
+            id: socket.id.substring(0, 5), // Short ID for display
+            message: msg,
+            timestamp: new Date()
+        });
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
